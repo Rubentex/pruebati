81a830fa58d627a9eaeafd16dd56440b4b60dd3a
Rubentex <77239189+Rubentex@users.noreply.github.com>
2022-05-23 18:41:42 -0400

    sss

--- fra/script.js
+++ fra/script.js
@@ -1,26 +1,42 @@
-if (location.protocol === "file:") {
-    document.getElementById("webserver-warning").style.display = "block";
-  }
+var config = {
+        key: 'c6d7e091-e62e-4bda-819f-99bb13506fba',
+        logs: {
+          level: 'debug'
+        },
+        playback: {
+          preferredTech: [
+            {
+          player: 'html5',
+          streaming: 'Dash'
+        }, {
+          player: 'html5',
+          streaming:'hls'
+        }, {
+          player: 'html5',
+          streaming: 'smooth'
+        }, {
+          player: 'native',
+          streaming: 'hls'
+        }, {
+          player: 'native',
+          streaming: 'progressive'
+        }]
+        }
+    };
 
-  var player = bitdash("player");
-
-  var conf = {
-    key:              "11d3698c-efdf-42f1-8769-54663995de2b",
-    source: {
-      dash:           "",
-      hls:            "https://spliktv-nhl004@d21gtq1mno4siw.cloudfront.net/out/v1/c7889d78bfa643dc9079c46c81be4b6d/index.m3u8"
-    },
-    style: {
-      width:          "100%",
-      aspectratio:    "16:9",
-      controls:       true
+    var source = {
+        hls: 'https://spliktv-nhl004@d21gtq1mno4siw.cloudfront.net/out/v1/c7889d78bfa643dc9079c46c81be4b6d/index.m3u8'
     }
-  };
 
-  player.setup(conf).then(function(value) {
-    // Success
-    console.log("Successfully created bitmovin player instance");
-  }, function(reason) {
-    // Error!
-    console.log("Error while creating bitmovin player instance");
-  });
+    var container = document.getElementById('my-player');
+    var player = new bitmovin.player.Player(container, config);
+    player.load(source).then(
+        function(value) {
+            // Success
+            console.log('Successfully created bitmovin player instance');
+        },
+        function(reason) {
+            // Error!
+            console.log('Error while creating bitmovin player instance');
+        }
+    );
